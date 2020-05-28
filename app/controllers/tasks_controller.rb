503eef05bc7f49562f6482c72fe5c332fd10a5b1
wako-mori <63290071+wako-mori@users.noreply.github.com>
2020-05-28 21:06:08 +0900

    タスク登録機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,29 @@
 class TasksController < ApplicationController
+  before_action :set_group
+
   def index
+    @task = Task.new
+    @tasks = @group.tasks.includes(:user)
+  end
+
+  def create
+    @task = @group.tasks.new(task_params)
+    if @task.save
+      redirect_to group_tasks_path(@group), notice: 'タスクが追加されました'
+    else
+      @taskus = @group.tasks.includes(:user)
+      flash.now[:alert] = 'タスク名を入力してください'
+      render :index
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:content, :explanation, :limit, :excution).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
---
+++ app/models/task.rb
@@ -0,0 +1,6 @@
+class Task < ApplicationRecord
+  belongs_to :group
+  belongs_to :user
+
+  validates :content, presence: true
+end
