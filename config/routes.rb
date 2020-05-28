b92b553665cd5216594974435c6f09498d6effd2
wako-mori <63290071+wako-mori@users.noreply.github.com>
2020-05-28 17:34:09 +0900

    group機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-
   root "tasks#index"
+
   resources :users, only: [:edit, :update]
 
+  resources :groups, only: [:new, :create, :edit, :update]
 end
