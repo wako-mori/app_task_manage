24e86fa9ab2e3f63de42c2e78586d7a65fcc8492
wako-mori <63290071+wako-mori@users.noreply.github.com>
2020-06-07 16:17:25 +0900

    グループ追加時のエラー解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, uniqueness: true
-  validates :password, presence: true, length: {minimum: 8 }
+  # validates :password, presence: true, length: {minimum: 8 }
 
   has_many :group_users
   has_many :groups, through: :group_users
--- db/schema.rb
+++ db/schema.rb
@@ -50,9 +50,7 @@ ActiveRecord::Schema.define(version: 20200528084736) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "group_users", "groups"
